523a42374e8ca99ffe486377c173787726c0cbdd
benmagill <benmagill@protonmail.com>
2024-06-27 17:21:40 +0100

    finish DPLL algorithm

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,29 @@ use std::collections::HashMap;
 use parser::*;
 use solver::{apply, cnf};
 
-use crate::solver::to_clauses;
+use crate::solver::{dpll, to_clauses};
 
 fn main() {
     let example = "(p ∧ q) ∨";
     let ex2 = format!("(p {OR} q) {AND} (p {OR} r)");
     let ex3 = format!("{NOT}p {OR} {NOT} {NOT}q {OR} r {IMPL} z");
     let ex4 = format!("(p {OR} q) {OR} (r {OR} q)");
-    let ex5 = format!("(p {OR} (q {OR} r)) {AND} (((p {AND} q) {OR} r) {AND} {NOT}z)");
+    let ex5 = format!("(p) {AND} (z) {AND} ( {NOT}z)");
     let ex6 = format!("{NOT}p {IMPL} (p {IMPL} {NOT}(p {OR} p))");
+    let ex7 = format!("(p {OR} (q {OR} r)) {AND} (((p {AND} q) {OR} r) {AND} {NOT}z)");
+    let ex8 = format!("{NOT}(p {AND} q {IMPL} q {AND} r)");
 
     // Parsed input must have no whitespace
-    let in_chars: String = ex5.split_whitespace().collect();
+    let in_chars: String = ex8.split_whitespace().collect();
     println!("Parsing: {}", &in_chars);
     let tree = (parser(&in_chars));
 
     let tree_0 = tree.get(0).unwrap().clone();
     cnf(tree_0.clone());
     dbg!(&tree_0);
-    dbg!(to_clauses(tree_0.clone()));
+    let c = to_clauses(tree_0.clone());
+
+    dbg!(dpll(c));
 
     let mut map = HashMap::new();
     map.insert('p', true);
--- src/solver.rs
+++ src/solver.rs
@@ -4,15 +4,34 @@
 // solving and returning the asignment of variables
 // //
 
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+    ops::Not,
+    rc::Rc,
+};
 
 use crate::parser::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Variable {
     name: char,
     negated: bool,
 }
+
+impl Not for &Variable {
+    type Output = Variable;
+
+    fn not(self) -> Self::Output {
+        Variable {
+            name: self.name,
+            negated: !self.negated,
+        }
+    }
+}
+
+type Clause = Vec<Variable>;
+type CNF = Vec<Clause>;
 
 pub fn apply(tree: NodeRef, assignments: &HashMap<char, bool>) -> bool {
     // Collapse tree to a single truth value
@@ -196,7 +215,7 @@ pub fn cnf(tree: NodeRef) {
     // TODO: convert to lists of clauses
 }
 
-pub fn to_clauses(tree: NodeRef) -> Vec<Vec<Variable>> {
+pub fn to_clauses(tree: NodeRef) -> CNF {
     // When in CNF, all ANDs are at the top before any other symbols
     let no_and = remove_and(tree);
 
@@ -206,7 +225,7 @@ pub fn to_clauses(tree: NodeRef) -> Vec<Vec<Variable>> {
             remove_or(c)
                 .iter()
                 .map(|n| {
-                    let mut value = n.borrow().value;
+                    let value = n.borrow().value;
                     if n.borrow().value == NOT {
                         Variable {
                             name: n.borrow().left.clone().unwrap().borrow().value,
@@ -259,5 +278,162 @@ fn remove_and(tree: NodeRef) -> Vec<NodeRef> {
         v
     } else {
         return vec![tree.clone()];
+    }
+}
+
+fn simplify_where_true(clauses: &RefCell<Vec<Clause>>, var: &Variable) {
+    let mut new_clauses: Vec<Vec<Variable>> = Vec::new();
+    for c in clauses.borrow().iter() {
+        if c.contains(var) {
+            continue;
+        } else if c.contains(&!var) {
+            // Remove negative version of unit from clauses
+            let i = c.into_iter().clone().filter(|&v| v != &!var);
+            let new_c = i.map(|v| v.to_owned());
+            new_clauses.push(new_c.collect());
+        } else {
+            new_clauses.push(c.to_owned());
+        }
+    }
+    clauses.replace(new_clauses);
+}
+
+pub fn dpll(clauses: CNF) -> Option<Vec<Variable>> {
+    let mut solution = vec![];
+    // TODO: in a loop
+    // remove tautologies
+    // handle unit clauses
+    // handle pure literals
+    // check if t/f
+    // case split
+    dbg!(&clauses);
+
+    let mut clauses = clauses.clone();
+
+    // Remove tautologies
+    clauses.retain(|c| !is_tautology(c));
+
+    // Remove unit clauses
+    let mut clauses = RefCell::new(clauses.clone());
+    loop {
+        let unit_clause = clauses
+            .borrow_mut()
+            .clone()
+            .into_iter()
+            .find(|c| c.len() == 1);
+
+        if let Some(unit) = unit_clause {
+            let unit = unit.get(0).unwrap();
+            println!(
+                "Removing unit {}{}",
+                if unit.negated { "¬" } else { "" },
+                unit.name
+            );
+            solution.push(unit.clone());
+            // remove all negations inlcuding the unit variable
+            simplify_where_true(&clauses, unit);
+            //let mut new_clauses: Vec<Vec<Variable>> = Vec::new();
+            //for c in clauses.borrow().iter() {
+            //if c.contains(unit) {
+            //continue;
+            //} else if c.contains(&!unit) {
+            //// Remove negative version of unit from clauses
+            //let i = c.into_iter().clone().filter(|&v| v != &!unit);
+            //let new_c = i.map(|v| v.to_owned());
+            //new_clauses.push(new_c.collect());
+            //} else {
+            //new_clauses.push(c.to_owned());
+            //}
+            //}
+            //clauses.replace(new_clauses);
+        } else {
+            // No units in clauses
+            break;
+        }
+        dbg!(&clauses);
+    }
+
+    // Remove pure literals
+    // Get all literals (incl negations)
+    let c = clauses.borrow().clone();
+    let u = unique_variables(&c);
+
+    // Determine if any dont include their negation
+    //let mut pure = vec![];
+    for v in u.clone() {
+        if !u.contains(&&!v) {
+            solution.push(v.to_owned());
+            // Remove clauses with pure literals
+            clauses.borrow_mut().retain(|c| !c.contains(v))
+        }
+    }
+
+    dbg!(&clauses);
+
+    if not_valid(clauses.borrow().clone()) {
+        None
+    } else if clauses.borrow().clone().len() == 0 {
+        Some(solution)
+    } else {
+        println!("DOING CASE SPLIT");
+        // Select any variable v
+        let c = clauses.borrow().clone();
+        let v = c.get(0).unwrap().get(0).unwrap();
+        // Case T = remove clauses with v, and remove ¬v from clauses
+        let t = &clauses.clone();
+        simplify_where_true(&clauses.clone(), v);
+        let tr = dpll(t.borrow().clone());
+        // Case F = remove clauses with ¬v, and remove v from clauses
+        let f = &clauses.clone();
+        simplify_where_true(&clauses.clone(), &!v);
+        let fr = dpll(f.borrow().clone());
+
+        if let Some(mut partial_solution) = tr {
+            solution.append(&mut partial_solution);
+            Some(solution)
+        } else if let Some(mut partial_solution) = fr {
+            solution.append(&mut partial_solution);
+            Some(solution)
+        } else {
+            None
+        }
+    }
+}
+
+fn unique_variables(clauses: &Vec<Clause>) -> Vec<&Variable> {
+    let mut h = HashSet::new();
+
+    let mut flat: Vec<&Variable> = clauses.iter().flatten().collect();
+
+    flat.retain(|&v| h.insert(v));
+
+    flat
+}
+
+fn is_unit(clause: &Clause) -> Option<Variable> {
+    if clause.len() == 1 {
+        return Some(clause.get(0).unwrap().clone());
+    }
+
+    None
+}
+
+fn is_tautology(clause: &Clause) -> bool {
+    // TODO: very inefficient
+    for c in clause {
+        for c2 in clause {
+            if c.name == c2.name && c.negated != c2.negated {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+fn not_valid(clauses: Vec<Clause>) -> bool {
+    match clauses.into_iter().find(|c| c.len() == 0) {
+        Some(_) => true,
+        None => false,
     }
 }
